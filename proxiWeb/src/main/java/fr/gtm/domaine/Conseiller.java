46d11cc01f47ebbe016deb2d67d5c1a5a1062263
Nabs1012 <n.sersoub@hotmail.fr>
2018-04-25 09:43:02 +0200

    ajout de la futur partie clientWeb

---
+++ proxiWeb/src/main/java/fr/gtm/domaine/Conseiller.java
@@ -0,0 +1,45 @@
+package fr.gtm.domaine;
+
+public class Conseiller {
+private int idConseiller;
+private String nom;
+private String prenom;
+/**
+ * Constructeur
+ * @param idConseiller
+ * @param nom
+ * @param prenom
+ */
+public Conseiller(int idConseiller, String nom, String prenom) {
+	super();
+	this.idConseiller = idConseiller;
+	this.nom = nom;
+	this.prenom = prenom;
+}
+/**
+ * Constructeur vide
+ */
+public Conseiller() {
+	super();
+}
+public int getIdConseiller() {
+	return idConseiller;
+}
+public void setIdConseiller(int idConseiller) {
+	this.idConseiller = idConseiller;
+}
+public String getNom() {
+	return nom;
+}
+public void setNom(String nom) {
+	this.nom = nom;
+}
+public String getPrenom() {
+	return prenom;
+}
+public void setPrenom(String prenom) {
+	this.prenom = prenom;
+}
+
+
+}
---
+++ proxiWeb/src/main/java/fr/gtm/domaine/Login.java
@@ -0,0 +1,55 @@
+package fr.gtm.domaine;
+
+public class Login {
+private int idLogin;
+private String login;
+private String motDePasse;
+private int idConseiller;
+/**
+ * Constructeur
+ * @param idLogin
+ * @param login
+ * @param motDePasse
+ * @param idConseiller
+ */
+public Login(int idLogin, String login, String motDePasse, int idConseiller) {
+	super();
+	this.idLogin = idLogin;
+	this.login = login;
+	this.motDePasse = motDePasse;
+	this.idConseiller = idConseiller;
+}
+/**
+ * Constructeur vide
+ */
+public Login() {
+	super();
+}
+public int getIdLogin() {
+	return idLogin;
+}
+public void setIdLogin(int idLogin) {
+	this.idLogin = idLogin;
+}
+public String getLogin() {
+	return login;
+}
+public void setLogin(String login) {
+	this.login = login;
+}
+public String getMotDePasse() {
+	return motDePasse;
+}
+public void setMotDePasse(String motDePasse) {
+	this.motDePasse = motDePasse;
+}
+public int getIdConseiller() {
+	return idConseiller;
+}
+public void setIdConseiller(int idConseiller) {
+	this.idConseiller = idConseiller;
+}
+
+
+
+}
